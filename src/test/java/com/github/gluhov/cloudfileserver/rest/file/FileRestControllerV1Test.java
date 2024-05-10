0c8f3b6f432ad5a45fd5ae2fa688286144b5ce5a
gluhov-d <glukhov.d@gmail.com>
2024-05-10 16:29:12 +0300

    add rest controller few tests

--- src/test/java/com/github/gluhov/cloudfileserver/rest/file/FileRestControllerV1Test.java
+++ src/test/java/com/github/gluhov/cloudfileserver/rest/file/FileRestControllerV1Test.java
@@ -3,29 +3,39 @@ package com.github.gluhov.cloudfileserver.rest.file;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.gluhov.cloudfileserver.config.WebSecurityConfig;
+import com.github.gluhov.cloudfileserver.dto.AuthRequestDto;
 import com.github.gluhov.cloudfileserver.dto.FileEntityDto;
 import com.github.gluhov.cloudfileserver.mapper.FileEntityMapper;
 import com.github.gluhov.cloudfileserver.mapper.UserMapper;
+import com.github.gluhov.cloudfileserver.repository.UserRepository;
 import com.github.gluhov.cloudfileserver.rest.AuthRestControllerV1;
 import com.github.gluhov.cloudfileserver.rest.FileRestControllerV1;
+import com.github.gluhov.cloudfileserver.security.AuthenticationManager;
 import com.github.gluhov.cloudfileserver.security.SecurityService;
 import com.github.gluhov.cloudfileserver.service.FileEntityService;
 import com.github.gluhov.cloudfileserver.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 import static com.github.gluhov.cloudfileserver.rest.file.FileEntityTestData.fileUser;
+import static com.github.gluhov.cloudfileserver.rest.user.UserTestData.moderator;
+import static org.mockito.ArgumentMatchers.any;
 
 @ComponentScan({"com.github.gluhov.cloudfileserver.errorhandling"})
 @ActiveProfiles("test")
+@Import(WebSecurityConfig.class)
 @WebFluxTest(controllers = {FileRestControllerV1.class, AuthRestControllerV1.class})
 @TestPropertySource("classpath:application-test.yaml")
 class FileRestControllerV1Test {
@@ -52,11 +62,24 @@ class FileRestControllerV1Test {
     @MockBean
     private FileEntityService fileEntityService;
 
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
     private String token;
 
     @BeforeEach
     public void setUp() {
         if ( token == null) {
+            AuthRequestDto dto = new AuthRequestDto();
+            dto.setPassword("12354");
+            dto.setUsername("moderator_user");
+            BDDMockito.given(userService.getUserByUserName(any()))
+                    .willReturn(Mono.just(moderator));
+            BDDMockito.given(userRepository.findByUsername(any()))
+                    .willReturn(Mono.just(moderator));
             String resp = webTestClient.post()
                     .uri(LOGIN_URL)
                     .contentType(MediaType.APPLICATION_JSON)
--- src/test/java/com/github/gluhov/cloudfileserver/rest/user/AuthRestControllerV1Test.java
+++ src/test/java/com/github/gluhov/cloudfileserver/rest/user/AuthRestControllerV1Test.java
@@ -1,26 +1,53 @@
 package com.github.gluhov.cloudfileserver.rest.user;
 
+import com.github.gluhov.cloudfileserver.config.WebSecurityConfig;
+import com.github.gluhov.cloudfileserver.dto.AuthRequestDto;
+import com.github.gluhov.cloudfileserver.dto.UserDto;
+import com.github.gluhov.cloudfileserver.mapper.UserMapper;
 import com.github.gluhov.cloudfileserver.rest.AuthRestControllerV1;
+import com.github.gluhov.cloudfileserver.security.AuthenticationManager;
+import com.github.gluhov.cloudfileserver.security.SecurityService;
 import com.github.gluhov.cloudfileserver.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest
-@AutoConfigureWebTestClient
+import static com.github.gluhov.cloudfileserver.rest.user.UserTestData.moderator;
+import static com.github.gluhov.cloudfileserver.rest.user.UserTestData.user;
+import static org.mockito.ArgumentMatchers.any;
+
+@ComponentScan({"com.github.gluhov.cloudfileserver.errorhandling"})
 @ActiveProfiles("test")
+@WebFluxTest(controllers = {AuthRestControllerV1.class})
+@Import(WebSecurityConfig.class)
+@TestPropertySource("classpath:application-test.yaml")
 class AuthRestControllerV1Test {
 
     private static final String REST_URL = AuthRestControllerV1.REST_URL;
+
     @Autowired
     private WebTestClient webTestClient;
 
-    @Mock
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
     private UserService userService;
+
+    @MockBean
+    private SecurityService securityService;
+
+    @MockBean
+    private UserMapper userMapper;
 
     @Test
     void register() {
@@ -28,5 +55,18 @@ class AuthRestControllerV1Test {
 
     @Test
     void login() {
+        AuthRequestDto dto = new AuthRequestDto();
+        dto.setPassword("12354");
+        dto.setUsername("moderator_user");
+        BDDMockito.given(userService.getUserByUserName(any()))
+                        .willReturn(Mono.just(moderator));
+        webTestClient.post()
+                .uri(REST_URL + "/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UserDto.class)
+                .isEqualTo(new UserDto(user));
     }
 }
